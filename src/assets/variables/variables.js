6834dc05f2b88595e207d70b3a1c2b230d97066a
Gloria R <norimaqidoe@hotmail.com>
2025-02-14 21:04:48 -0600

    feat: add variable for rendering and changing images folder

---
+++ src/assets/variables/variables.js
@@ -0,0 +1,36 @@
+import colton from '@/assets/images/images-colton.jpg';
+import irene from '@/assets/images/images-irene.jpg';
+import anne from '@/assets/images/images-anne.jpg';
+
+export const ratings = [
+    { id: 1, text: "Rated 5 Stars in Reviews" },
+    { id: 2, text: "Rated 5 Stars in Report Guru" },
+    { id: 3, text: "Rated 5 Stars in BestTech" },
+];
+
+export const reviews = [
+    {
+      id: 1,
+      name: "Colton Smith",
+      role: "Verified Buyer",
+      image: colton,
+      review:
+        "We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!",
+    },
+    {
+      id: 2,
+      name: "Irene Roberts",
+      role: "Verified Buyer",
+      image: irene,
+      review:
+        "Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.",
+    },
+    {
+      id: 3,
+      name: "Anne Wallace",
+      role: "Verified Buyer",
+      image: anne,
+      review:
+        "Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!",
+    },
+];
